059c017d003732a704eb7d852b049bac246dbf9d
Stephan Spindler <stephan.spindler@device-insight.com>
2019-07-11 11:06:16 +0200

    Release version 1.1.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='csbootstrap',
-    version='1.1.0a1',
+    version='1.1.0',
     packages=['csbootstrap'],
     entry_points={
         'console_scripts': [
